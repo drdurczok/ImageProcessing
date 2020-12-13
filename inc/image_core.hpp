5a51c67ce66b5c88eec80579b71319315d215aba
Dan <ddurczok@sii.pl>
2020-12-13 10:09:50 +0100

    Improved image handling

--- inc/image_core.hpp
+++ inc/image_core.hpp
@@ -5,14 +5,25 @@
 #include <opencv2/imgcodecs.hpp>
 #include <stdint.h>
 
+using namespace std;
+
 class image_core {
   public:
-  	image_core();
-  	cv::Mat load_image(std::string);
-  	void display_image(std::string Handle, cv::Mat);
-  	void save_image(std::string, cv::Mat);
+  	image_core(uint8_t, uint8_t);
+  	cv::Mat load_image(string);
+  	void display_image(uint8_t, cv::Mat);
+  	void save_image(string, cv::Mat);
 	cv::Mat multiply_saturation_greyscale(cv::Mat, float);
 	cv::Mat multiply_saturation_color(cv::Mat, float, int);
+	cv::Mat get_DFT(cv::Mat, bool);
+
+  private:
+  	uint16_t screen_res_x = 1080;
+  	uint16_t screen_res_y = 720;
+
+  	void prepare_windows(uint8_t, uint8_t);
+
+
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@ using namespace std;
 
 int main(){
 	string path_to_image = "../samples/images/IMG_001.jpg";
-	image_core img_core;
+	image_core img_core(2,2);
+	
 	Mat image = img_core.load_image(path_to_image);
-	img_core.display_image("01", image);
-	img_core.save_image("temp.jpg",image);
+	img_core.display_image(0, image);
+	//img_core.save_image("temp.jpg",image);
 
 	cv::Mat mod_image = img_core.multiply_saturation_color(image, 0.5, 0);
-    img_core.display_image("02", mod_image);
+    img_core.display_image(1, mod_image);
+
+    //img_core.get_DFT(image);
 
     waitKey(0); // Wait for a keystroke in the window
     return 0;
