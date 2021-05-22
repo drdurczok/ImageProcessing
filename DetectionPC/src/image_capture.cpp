91e4b2b158140849dc4dd46fac89ea0ab31ff457
drdurczok <drdurczok@gmail.com>
2021-05-22 13:21:46 +0000

    Fixed video stream for ARM

--- DetectionPC/src/image_capture.cpp
+++ DetectionPC/src/image_capture.cpp
@@ -5,12 +5,18 @@ image_capture::image_capture(){
     // open the default camera using default API cap.open(0);
     // OR advance usage: select any API backend
     int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    //int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    int apiID = cv::CAP_GSTREAMER;
     // open selected camera using selected API
     cap.open(deviceID, apiID);
+    cap.set(CAP_PROP_FRAME_WIDTH, 320);
+    cap.set(CAP_PROP_FRAME_HEIGHT,240);
     // check if we succeeded
     if (!cap.isOpened()) {
         cout << "ERROR: Unable to open camera." << endl;
+    }
+    else{
+        cout << "INFO: Camera opened successfully." << endl;
     }
 
     // Prepare folder for saving images
@@ -28,7 +34,7 @@ image_capture::~image_capture(){
  */
 Mat image_capture::take_image(){
 	uint i = 0;
-	uint max_tries = 100;
+	uint max_tries = 10;
 
 	while(i < max_tries){
 	    // wait for a new frame from camera and store it into 'frame'
